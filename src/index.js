c498895c0e70e9b987ed1e6d91f6ff537f937936
Jordy M <jordimontero01@bitbucket.org>
2024-09-03 15:47:57 -0500

    ahoraa

--- src/index.js
+++ src/index.js
@@ -1,25 +1,13 @@
-import React, { useState } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import StarRating from './StarRating';
-
-function Test() {
-  const [movieRating, setMovieRating] = useState()
-  return (
-    <div>
-      <StarRating color='blue' onSetRating={setMovieRating} />
-      <p>{movieRating ? `Hola esta pelicula fue puntuada con ${movieRating} estrellas` : 'Porfavor puntua esta Pelicula'}</p>
-      <StarRating color='blue' onSetRating={setMovieRating} />
-    </div>
-  )
-}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <React>
     <App />
 
-  </React.StrictMode>
+  </React>
 );
 
